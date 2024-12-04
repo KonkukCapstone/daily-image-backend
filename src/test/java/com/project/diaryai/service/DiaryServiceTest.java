59c6a9ae550d6ab397a32779e907e582361db1cf
pakaheo <k70923@konkuk.ac.kr>
2024-12-04 10:24:08 +0900

    test : DiaryService 테스트 틀 구성

--- src/test/java/com/project/diaryai/service/DiaryServiceTest.java
+++ src/test/java/com/project/diaryai/service/DiaryServiceTest.java
@@ -10,11 +10,5 @@ import org.springframework.boot.test.context.SpringBootTest;
 public class DiaryServiceTest {
     @Autowired
     private DiaryService diaryService;
-
-    @Test
-    void 이미지_생성_테스트() {
-        String diaryContent = "오늘은 산책을 다녀왔다.";
-        String imageUrl = diaryService.generateImage(diaryContent);
-        assertThat(imageUrl).isNotNull();
-    }
+    
 }
