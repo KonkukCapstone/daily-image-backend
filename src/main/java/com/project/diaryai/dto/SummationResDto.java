4284a9b383461e858c2f3935815c7104052d24fe
pakaheo <k70923@konkuk.ac.kr>
2024-12-09 16:13:11 +0900

    chore : 코드 주석 추가

--- src/main/java/com/project/diaryai/dto/SummationResDto.java
+++ src/main/java/com/project/diaryai/dto/SummationResDto.java
@@ -8,7 +8,7 @@ import lombok.Getter;
 public class SummationResDto {
 
     private String diarySummation; // 요약된 일기 내용
-    private String emotionKeyword;
+    private String emotionKeyword; // 키워드
     private String activityKeyword;
     private String locationKeyword;
 }
