c0aa6a7c48a47633a153792d6012cf16591b1b5e
pakaheo <k70923@konkuk.ac.kr>
2024-12-10 13:56:06 +0900

    feat : Create web configuration

---
+++ src/main/java/com/project/diaryai/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.project.diaryai.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://13.125.31.61:8080")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
